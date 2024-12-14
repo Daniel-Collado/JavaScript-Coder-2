e11380226b13867ea480e62ee811dfbe8aff2bf5
Daniel-Collado <danielcollado82@gmail.com>
2024-12-14 19:52:11 -0300

    Se agregaron las imagenes desde el archivo JSON y se modificó TODO el javascript para que en el html solo quede el contenedor row

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 /*jshint esversion: 6 */
 
+const contenedor = document.getElementsByClassName("row");
+
 const arrayElementos = [
     {
         id: 1,
@@ -39,9 +41,103 @@ const arrayElementos = [
     },
 ];
 
-console.log(arrayElementos[3]);
 
 
+const contenedorCards = document.createElement("div");
+contenedorCards.setAttribute("id", "contenedorCards");
+contenedorCards.className = "col-sm-12 col-md-6 col-lg-4 estiloCards";
+
+
+
+/*const cards = [
+    {
+    imagen: "img1.jpeg",
+    titulo: "Card title 1",
+    precio: "2000",
+    cantidad: "1",
+},
+    {
+    imagen: "img2.jpeg",
+    titulo: "Card title 1",
+    precio: "2000",
+    cantidad: "1",
+},
+];*/
+
+fetch('JSON/imagenes.json')
+.then(response => response.json())
+.then(data => {
+    const imagenes = data.imagenes;
+    arrayElementos.forEach((producto, indice) => {
+    
+    const imagen = imagenes[indice];
+    producto.imagen = imagen.ruta;
+    crearCard(producto);
+    });
+})
+.catch(error => console.log('Error al leer el archivo JSON:', error));
+
+function crearCard(producto){
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";  
+    
+    const imagen = document.createElement("img");
+    imagen.src = producto.imagen;
+    imagen.className = "card-img-top";
+    imagen.alt = "foto producto";
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const titulo = document.createElement("h5");
+    titulo.className = "card-title";
+    titulo.textContent = producto.nombre;
+
+    const precio = document.createElement("p");
+    precio.className = "card-text";
+    precio.textContent = `$${producto.precio}`;
+
+    const boton = document.createElement("a");
+    boton.href = "#";
+    boton.className = "btn btn-primary";
+    boton.textContent = "Ir a algún lado";
+    
+    const inputCantidad = document.createElement("input");
+    inputCantidad.type = "number";
+    inputCantidad.placeholder = "cantidad";
+    inputCantidad.className = "form-control";
+
+    cardBody.appendChild(titulo);
+    cardBody.appendChild(precio);
+    cardBody.appendChild(boton);
+    cardBody.appendChild(inputCantidad);
+    cardElement.appendChild(imagen);
+    cardElement.appendChild(cardBody);
+    contenedorCards.appendChild(cardElement);
+    contenedor[0].appendChild(contenedorCards);
+}
+
+
+
+/*arrayElementos.forEach((el) => {
+    crearCard(el);
+    
+    
+});
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const cards = document.querySelectorAll('.card');
 arrayElementos.forEach((el, indice) => {
     const card = cards[indice];
@@ -65,7 +161,7 @@ fetch('JSON/imagenes.json')
     });
 })
 .catch(error => console.log('Error al leer el archivo JSON:', error));
-
+*/
 
 
 
@@ -76,5 +172,5 @@ if (JSON.parse(localStorage.getItem("carrito"))) {
 }
 
 
-const contenedorCards = document.getElementById('contenedorCards');
+//const contenedorCards = document.getElementById('contenedorCards');
 
